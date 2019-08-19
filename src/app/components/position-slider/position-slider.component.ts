a647c6742226fd974ecf227ae8f02bf586de9016
Unknown <kayvanbree@gmail.com>
2019-08-19 17:15:33 +0200

    First version of using the position slider to seek position

--- src/app/components/position-slider/position-slider.component.ts
+++ src/app/components/position-slider/position-slider.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {PlayerStateModel} from '../../store/models/player-state.model';
 import {Store} from '@ngxs/store';
+import {Seek} from '../../store/actions/player.actions';
 
 @Component({
   selector: 'app-position-slider',
@@ -9,12 +10,24 @@ import {Store} from '@ngxs/store';
 })
 export class PositionSliderComponent implements OnInit {
   public player: PlayerStateModel;
+  private sliding = false;
 
   constructor(private store: Store) { }
 
   ngOnInit() {
     this.store.select(state => state.player).subscribe((value) => {
-      this.player = value;
+      if (!this.sliding) {
+        this.player = value;
+      }
     });
+  }
+
+  seekPosition(position: number) {
+    this.store.dispatch(new Seek(position));
+    this.sliding = false;
+  }
+
+  onInput(event: any) {
+    this.sliding = true;
   }
 }
