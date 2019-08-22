672fdd1fc9991be1b9496e3408c51edf00df0586
Unknown <kayvanbree@gmail.com>
2019-08-22 20:47:06 +0200

    Reduce album state size

--- src/app/store/actions/album.actions.ts
+++ src/app/store/actions/album.actions.ts
@@ -1,30 +1,3 @@
-import {AlbumModel} from '../models/album.model';
-import {ArtistModel} from '../models/artist.model';
-
-export class GetAlbums {
-  static readonly type = '[Album] GetAlbums';
-  constructor(
-    public ids: string[],
-    public page: number,
-    public pageSize: number
-  ) {}
-}
-
-export class GetAlbumsSuccess {
-  static readonly type = '[Album] GetAlbumsSuccess';
-  constructor(public albums: AlbumModel[]) {}
-}
-
-export class GetAlbum {
-  static readonly type = '[Album] GetAlbum';
-  constructor(public id: string) {}
-}
-
-export class GetAlbumSuccess {
-  static readonly type = '[Album] GetAlbumSuccess';
-  constructor(public album: AlbumModel) {}
-}
-
 export class SaveAlbum {
   static readonly type = '[Album] SaveAlbum';
   constructor(public id: string) {}
